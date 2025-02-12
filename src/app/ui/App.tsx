25c1a0fc09a58f0fe1dedc776fdc42c6fc6041bd
Nikolay <kolechka.malyshev.04@gmail.com>
2025-02-13 01:26:17 +0700

    add black-theme

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -2,7 +2,11 @@ import { FC } from 'react';
 import { AppRouter } from '@app/lib/routes';
 
 const App: FC = () => {
-  return <AppRouter />;
+  return (
+    <div className={'dark bg-secondary min-h-screen'}>
+      <AppRouter />
+    </div>
+  );
 };
 
 export default App;
